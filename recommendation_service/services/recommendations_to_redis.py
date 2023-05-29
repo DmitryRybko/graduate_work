cdafcdced8748eab378d1fd122e09ecf55831db0
Dmitry R <dmr.main@outlook.com>
2023-05-29 10:59:48 +0300

    recommendations are pushed to Redis on a periodic basis

--- recommendation_service/services/recommendations_to_redis.py
+++ recommendation_service/services/recommendations_to_redis.py
@@ -1,19 +1,31 @@
 import redis
 import json
-
+import datetime
 from recommendation_service.utils.get_recommendations import get_recommendations
 from recommendation_service.config import settings
 
-# create a Redis client
-r = redis.Redis(settings.redis_host, settings.redis_port, settings.redis_db)
 
-# define the data to be saved
-user_id = 'email2@emails.ru'
-movies = get_recommendations(user_id)
+def save_recommendations_for_users():
+    # create a Redis client
+    r = redis.Redis(settings.redis_host, settings.redis_port, settings.redis_db)
+
+    # define the data to be saved
+    user_id = 'email2@emails.ru'
+    movies = get_recommendations(user_id)
+
+    # convert the uuids list to a JSON-encoded string
+    movies_str = json.dumps(movies)
+
+    # save the data to Redis
+    r.set(user_id, movies_str)
 
 
-# convert the uuids list to a JSON-encoded string
-movies_str = json.dumps(movies)
+# set up a weekly timer to run the function
+week_interval = datetime.timedelta(weeks=1)
+last_run_time = datetime.datetime.now()
 
-# save the data to Redis
-r.set(user_id, movies_str)
+while True:
+    current_time = datetime.datetime.now()
+    if (current_time - last_run_time) >= week_interval:
+        save_recommendations_for_users()
+        last_run_time = current_time
