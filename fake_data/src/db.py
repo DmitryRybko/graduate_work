a92b6dc67845ce98e5c5e17bdb46da592bb0ebe3
Alex Kruglow <alexkruglow@gmail.com>
2023-06-02 01:59:25 +0300

    Endless generator was added.

--- fake_data/src/db.py
+++ fake_data/src/db.py
@@ -1,12 +1,14 @@
 """Db module."""
 
-import sqlalchemy as db
+from motor.motor_asyncio import AsyncIOMotorClient
 
-from settings import settings
+import sqlalchemy as db
 
 
 auth_db = None
 movies_db = None
+
+watching_history_db = None
 
 
 def get_auth_db():
@@ -19,14 +21,24 @@ def get_movies_db():
     return movies_db
 
 
+def get_watching_history_db():
+    """Return watching history DB."""
+    return watching_history_db
+
+
 def init_auth_db(url: str) -> None:
+    """Init connect to auth db."""
     global auth_db
     auth_db = db.create_engine(url)
 
 
 def init_movies_db(url: str) -> None:
+    """Init connect to movies db."""
     global movies_db
     movies_db = db.create_engine(url)
 
 
-init_auth_db(settings.auth_db_url)
+def init_watching_history_db(url: str, db_name: str) -> None:
+    """Init connect to watching history db."""
+    global watching_history_db
+    watching_history_db = AsyncIOMotorClient(url)[db_name]
--- fake_data/src/utils.py
+++ fake_data/src/utils.py
@@ -1,15 +1,110 @@
 """Utils module."""
 
+import random
+import time
 from time import sleep
+from contextlib import contextmanager
 
+from sqlalchemy.orm import sessionmaker
+
+import db
+from auth_db_utils import generate_a_new_user, generate_roles, generate_users
 from logger import get_logger
+from movies_db_utils import (
+    generate_a_new_film_work, generate_film_works,
+    generate_genres, generate_new_person, generate_persons
+)
+from settings import settings
+from watching_history_utils import (
+    generate_watching_history, generate_new_watched_film
+)
+
+logger = get_logger()
+
+
+@contextmanager
+def sqlalchemy_session(engine):
+    """Return session. It is context manager."""
+    Session = sessionmaker(bind=engine, autoflush=False)
+    session = Session()
+    yield session
+    session.close()
+
+
+async def generate_first_data():
+    """Generate firs part of fake data to test."""
+    auth_db = db.get_auth_db()
+    movies_db = db.get_movies_db()
+
+    # Wait auth_db
+    wait_db(
+        auth_db,
+        (
+            settings.user_table_name,
+            settings.role_table_name,
+            settings.log_history_table_name,
+            settings.user_role_table_name
+        )
+    )
+
+    wait_db(
+        movies_db,
+        (
+            settings.person_table_name,
+            settings.genre_table_name,
+            settings.person_film_work_table_name,
+            settings.genre_film_work_table_name,
+            settings.film_work_table_name,
+        ),
+        settings.movies_schema_name
+    )
+    with (
+        sqlalchemy_session(auth_db) as auth_session,
+        sqlalchemy_session(movies_db) as movies_session
+    ):
+
+        generate_roles(auth_session)
+        generate_users(auth_session)
+
+        generate_genres(movies_session)
+        generate_persons(movies_session)
+        generate_film_works(movies_session)
+
+        await generate_watching_history(auth_session, movies_session)
+
+
+async def generate_data_by_the_time() -> None:
+    """Generate one by one items in the project DB."""
+    auth_db = db.get_auth_db()
+    movies_db = db.get_movies_db()
+    watching_history_db = db.get_watching_history_db()
+
+    actions: tuple = ('film_work', 'person', 'user', 'watched_film')
+
+    while True:
+        with (
+            sqlalchemy_session(auth_db) as auth_session,
+            sqlalchemy_session(movies_db) as movies_session
+        ):
+            action = random.choice(actions)
+            if action == 'film_work':
+                logger
+                generate_a_new_film_work(movies_session)
+            elif action == 'person':
+                generate_new_person(movies_session)
+            elif action == 'user':
+                generate_a_new_user(auth_session)
+            elif action == 'watched_film':
+                await generate_new_watched_film(
+                    auth_session, movies_session, watching_history_db
+                )
+            time.sleep(random.randint(1, 5))
 
 
 def wait_db(
     engine, tables: tuple, schema: str = 'public', timeout_min: int = 2
 ):
     """Wait when all db and all tables are available."""
-    logger = get_logger()
     logger.debug(f'Engine: {engine}; Tables: {tables}; Timeout: {timeout_min}')
     timeout = timeout_min * 60
     for table in tables:
