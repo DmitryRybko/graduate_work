6ebdc9a8ec2bf64d352bfe02dfc8d58a4b7314b9
Dmitry R <dmr.main@outlook.com>
2023-06-03 11:55:52 +0300

    token parser is injected using Depends function

--- recommendation_service/api/v1/recommendations_api.py
+++ recommendation_service/api/v1/recommendations_api.py
@@ -1,11 +1,11 @@
 from dotenv import load_dotenv
-from fastapi import APIRouter, Response, Request
+from fastapi import APIRouter, Request, Depends
 from starlette import status
 from loguru import logger
 
 from recommendation_service.api.v1.schemas import RecommendationsResponse
 from recommendation_service.config import settings
-from recommendation_service.services.jwt_decode import decode_jwt
+from recommendation_service.services.jwt_decode import decode_jwt, ParseJWTToken
 from recommendation_service.utils.get_recom_from_redis import retrieve_recom_movies
 
 logger.add("debug.log", level=settings.log_level)
@@ -14,11 +14,11 @@ router = APIRouter()
 
 
 @router.get("/", response_model=RecommendationsResponse)
-def get_recommendations(request: Request, response: Response) -> RecommendationsResponse:
+def get_recommendations(request: Request, token_parser=Depends(ParseJWTToken)) -> RecommendationsResponse:
     # в составе JWT токена приезжает user_id (email), который мы декодируем и используем для запроса в database
     try:
         token: str = request.headers["Authorization"]
-        user_email: str | None = decode_jwt(token)
+        user_email: str | None = token_parser(token)
         logger.info(f"{user_email}")
     except KeyError:
         user_email = None
--- recommendation_service/services/jwt_decode.py
+++ recommendation_service/services/jwt_decode.py
@@ -1,10 +1,12 @@
 import jwt
 
 
-def decode_jwt(token: str) -> str:
+class ParseJWTToken:
     """Return user_id from token."""
-    token_clean: str = token.replace("Bearer ", "")
-    user_id: str = jwt.decode(
-        token_clean, options={"verify_signature": False}
-    )['sub']
-    return user_id
+    def __call__(self, jwt_token: str):
+        self.jwt_token = jwt_token
+        token_clean: str = self.jwt_token.replace("Bearer ", "")
+        user_id: str = jwt.decode(
+            token_clean, options={"verify_signature": False}
+        )['sub']
+        return user_id
