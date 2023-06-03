d6b7911d76a3f3d1329b0dcef9d2195cf5aff59d
Dmitry R <dmr.main@outlook.com>
2023-06-03 13:05:13 +0300

    saving recommended movies to Redis is now initiated if no recommendations are present

--- recommendation_service/services/recommendations_to_redis.py
+++ recommendation_service/services/recommendations_to_redis.py
@@ -9,10 +9,9 @@ from recommendation_service.db.redis import get_redis
 from recommendation_service.utils.get_recommendations import get_recommendations
 
 
-def save_recommendations_for_users(r: redis.Redis | None = Depends(get_redis)):
-    # define the data to be saved
+def save_recommendations_for_users(user: str, r: redis.Redis | None = Depends(get_redis)):
 
-    user_id: str = 'email2@emails.ru'
+    user_id: str = user
     movies: dict | None = get_recommendations(user_id)
 
     # convert the uuids list to a JSON-encoded string
@@ -23,14 +22,3 @@ def save_recommendations_for_users(r: redis.Redis | None = Depends(get_redis)):
         r.set(user_id, movies_str)
     else:
         raise Exception('Redis is not available')
-
-
-# set up a weekly timer to run the function
-week_interval = datetime.timedelta(weeks=1)
-last_run_time = datetime.datetime.now()
-
-while True:
-    current_time = datetime.datetime.now()
-    if (current_time - last_run_time) >= week_interval:
-        save_recommendations_for_users()
-        last_run_time = current_time
